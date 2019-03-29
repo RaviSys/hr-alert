7e7edc0767b9588ca60970d91137ba8ea0ebcc78
RaviSys <rksingh@isystango.com>
2019-03-29 13:35:06 +0530

    added header and footer to partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  def flash_class(level)
+    case level
+      when :success then "alert alert-success"
+      when :notice then "alert alert-success"
+      when :alert then "alert alert-danger"
+      when :error then "alert alert-error"
+      when :warning then "alert alert-warning"
+    end
+  end
+
   def resource_name
     :user
   end
