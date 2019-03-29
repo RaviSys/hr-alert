ae1b5dc0ac13be815709b2e810b0cf2f71adff9c
RaviSys <rksingh@isystango.com>
2019-03-29 12:52:14 +0530

    display session and registration forms together on custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  get 'login' => "auth#login"
   root "home#index"
 end
